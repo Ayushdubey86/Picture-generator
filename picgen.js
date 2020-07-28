38dff339042c5ab4fd08152e7d456d6d39b7180a
Ayushdubey86 <61616662+Ayushdubey86@users.noreply.github.com>
2020-07-29 02:28:35 +0530

    Add files via upload

---
+++ picgen.js
@@ -0,0 +1,6 @@
+function picgen() {
+  var image = document.createElement("img");
+  var h1 = document.getElementById("flex-pic-gen");
+  image.src = "https://picsum.photos/200";
+  h1.appendChild(image);
+}
